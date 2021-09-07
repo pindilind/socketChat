c3687bdf201af1700c4b028575716fa58a33dac0
pindilind <linda.gustafsson@outlook.com>
2021-09-07 11:09:44 +0200

    första bygget i server, samt anteckningsfil

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const server = express()
+const http = require('http').createServer(server)
+const io = require('socket.io')(http)
 const port = 3000
 
-server.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+server.use(express.static('public')) //servar ut publica filer
 
-server.listen(port, () => {
+io.on('connection', (socket) => {
+
+    console.log('new connection')
+}) 
+
+ /* server.get('/', (req, res) => {
+  res.send('Hello World!')
+}) */
+
+/* server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+}) */
+
+http.listen(port, () => console.log("Tjohooo! Det fungerade :) "))
