c35f2c6a542f026a9d7533a1d502ed6d61c48c7f
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-09-08 13:24:36 +0200

    contiune disconnection-function

--- public/public.js
+++ public/public.js
@@ -3,7 +3,18 @@ let name = ""
 
 window.onload = () => {
     name = prompt('Skriv ditt namn')
+
+    socket.emit("join", { name })
 }
+
+socket.on("joined", (incomingResult) => {
+    console.log(incomingResult.name + " joined the chat")
+
+    const msgList = document.getElementById('messages')
+    const msgListItem = document.createElement("li")
+    msgListItem.innerText = incomingResult.name + " joined the chat"
+    msgList.appendChild(msgListItem) 
+})
 
 socket.on('msgInput', (incomingResult) => {
 
@@ -13,10 +24,19 @@ socket.on('msgInput', (incomingResult) => {
     msgList.appendChild(msgListItem) 
 })
 
+socket.on('disconnected', () => {
+    console.log(name + " left the chat")
+}) 
+
+
 function submitMsg() {
     const input = document.getElementById("msgInput")
     const message = input.value 
     input.value = ""
 
     socket.emit('msgInput', { name, message })
+}
+
+function leaveChat() {
+    console.log("leave chat")
 }
